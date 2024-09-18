d014fc427e93e343649d3e647b57e6ee292538a7
Tuukka Koistinen <tvkoisti@gmail.com>
2024-09-18 15:12:26 +0300

    Fix install requirement list
    Remove from setup.py as it's defined in setup.cfg

--- setup.py
+++ setup.py
@@ -5,9 +5,6 @@ setup(
     description="chat-miner provides lean parsers for every major platform transforming chats into dataframes.\
          Artistic visualizations allow you to explore your data and create artwork from your chats.",
     packages=find_packages(),
-    install_requires=[
-        "argparse",
-    ],
     entry_points={
         "console_scripts": [
             "chatminer = chatminer.cli:main",
